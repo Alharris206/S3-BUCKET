a8329ae4c4037bc24136920e7301149d6d23b143
A.Harris <144071014+Alharris206@users.noreply.github.com>
2024-01-20 19:51:59 -0800

    Update 0-Auth.tf

--- 0-Auth.tf
+++ 0-Auth.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "aws" {
    region = "ap-northeast-3"
-   access_key = "AKIA4AVNGP3OMX2AJVXU"
-   secret_key = "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"
+   access_key = ""
+   secret_key = ""
 }
